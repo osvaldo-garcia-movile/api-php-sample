ffc9db3227c3c94da43ab23736629841ebc8c5d5
Osvaldo Garcia <aoga88@gmail.com>
2019-02-08 11:13:32 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#Instalation
+
+<ol>
+<li>composer install</li>
+<li>cp .env.example .env</li>
+<li>php send.php</li>
+</ol>
---
+++ send.php
@@ -0,0 +1,31 @@
+<?php
+require 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::create(__DIR__);
+$dotenv->load();
+
+$phone = '5512345678';
+$text = 'Mensaje de prueba';
+
+
+$dataRequest = [
+    'destination' => strlen($phone) == 10?'52' . $phone:$phone,
+    'messageText' => $text
+];
+
+$user = getenv('API_USER');
+$token = getenv('API_TOKEN');
+
+$client = new \GuzzleHttp\Client();
+
+echo json_encode($dataRequest) . "\n";
+
+$response = $client->request('POST', 'https://api-messaging.movile.com/v1/send-sms', [
+    'headers' => [
+        'authenticationtoken' => $token,
+        'username' => $user
+    ],
+    'json' => $dataRequest
+]);
+
+echo $response->getBody()->getContents() . "\n\n";
